32612a4ed042ea292bd2ccc6d07904ee1effb7c1
Fabian Buentello <fbuentelloapp@gmail.com>
2016-09-30 07:35:44 -0500

    init commit

---
+++ Talana/Talana.h
@@ -0,0 +1,19 @@
+//
+//  Talana.h
+//  Talana
+//
+//  Created by Fabian Buentello on 9/30/16.
+//  Copyright © 2016 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Talana.
+FOUNDATION_EXPORT double TalanaVersionNumber;
+
+//! Project version string for Talana.
+FOUNDATION_EXPORT const unsigned char TalanaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Talana/PublicHeader.h>
+
+
